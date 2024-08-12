b59baf943f1d44bb0464280f1dc1187b17462ff1
claudealdric <cvaldric@gmail.com>
2024-08-12 13:25:11 -0400

    Return a 404 status for missing players

--- server.go
+++ server.go
@@ -17,5 +17,9 @@ type PlayerServer struct {
 func (p *PlayerServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	url := req.URL.String()
 	player := strings.TrimPrefix(url, "/players/")
-	fmt.Fprint(rw, p.store.GetPlayerScore(player))
+	score := p.store.GetPlayerScore(player)
+
+	rw.WriteHeader(http.StatusNotFound)
+
+	fmt.Fprint(rw, score)
 }
