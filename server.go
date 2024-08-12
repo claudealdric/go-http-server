7f25c4eebd34ff2fc69ced87fc25ed962231d758
claudealdric <cvaldric@gmail.com>
2024-08-12 12:53:20 -0400

    Add functionality to return two specific players' score

--- server.go
+++ server.go
@@ -3,8 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func PlayerServer(rw http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(rw, "20")
+	url := req.URL.String()
+	player := strings.TrimPrefix(url, "/players/")
+
+	if player == "Pepper" {
+		fmt.Fprint(rw, "20")
+	}
+
+	if player == "Floyd" {
+		fmt.Fprint(rw, "10")
+	}
 }
