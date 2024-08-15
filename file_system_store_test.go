dae6acc7b4fefb1df7b5c47a400c66a981d0d5df
claudealdric <cvaldric@gmail.com>
2024-08-14 23:30:42 -0400

    Update the tape file type from `io.ReadWriteSeeker` to `os.File`

--- file_system_store_test.go
+++ file_system_store_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io"
 	"os"
 	"testing"
 )
@@ -59,7 +58,7 @@ func assertScoreEquals(t *testing.T, got, want int) {
 	}
 }
 
-func createTempFile(t testing.TB, initialData string) (io.ReadWriteSeeker, func()) {
+func createTempFile(t testing.TB, initialData string) (*os.File, func()) {
 	t.Helper()
 
 	tempFile, err := os.CreateTemp("", "db")
