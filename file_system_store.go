33f65678bac548dfd71968891ff3ba452489185a
claudealdric <cvaldric@gmail.com>
2024-08-14 23:23:36 -0400

    Create a new type to encapsulate the writing functionality

--- file_system_store.go
+++ file_system_store.go
@@ -6,14 +6,14 @@ import (
 )
 
 type FileSystemPlayerStore struct {
-	database io.ReadWriteSeeker
+	database io.Writer
 	league   League
 }
 
 func NewFileSystemStore(database io.ReadWriteSeeker) *FileSystemPlayerStore {
 	database.Seek(0, io.SeekStart)
 	league, _ := NewLeague(database)
-	return &FileSystemPlayerStore{database, league}
+	return &FileSystemPlayerStore{&tape{database}, league}
 }
 
 func (f *FileSystemPlayerStore) GetLeague() League {
@@ -37,6 +37,5 @@ func (f *FileSystemPlayerStore) RecordWin(name string) {
 		f.league = append(f.league, Player{name, 1})
 	}
 
-	f.database.Seek(0, io.SeekStart)
 	json.NewEncoder(f.database).Encode(f.league)
 }
