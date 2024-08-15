e3b01e47b9858620b2e5d4b2c82648f3c8d310b4
claudealdric <cvaldric@gmail.com>
2024-08-14 23:06:34 -0400

    Replace the in memory store with the file system store

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
+const dbFileName = "game.db.json"
+const port = 8080
+
 func main() {
-	server := NewPlayerServer(NewInMemoryPlayerStore())
-	log.Fatal(http.ListenAndServe(":8080", server))
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+	store := &FileSystemPlayerStore{db}
+	server := NewPlayerServer(store)
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), server); err != nil {
+		log.Fatalf("could not listen on port %d %v", port, err)
+	}
 }
