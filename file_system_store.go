0f51c8487b28d4ac00e78d519cd0a00466a96c92
claudealdric <cvaldric@gmail.com>
2024-08-14 22:51:56 -0400

    Refactor to use the newly created `League` method

--- file_system_store.go
+++ file_system_store.go
@@ -16,25 +16,19 @@ func (f *FileSystemPlayerStore) GetLeague() League {
 }
 
 func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
-	var wins int
-
-	for _, player := range f.GetLeague() {
-		if player.Name == name {
-			wins = player.Wins
-			break
-		}
+	player := f.GetLeague().Find(name)
+	if player == nil {
+		return 0
 	}
-
-	return wins
+	return player.Wins
 }
 
 func (f *FileSystemPlayerStore) RecordWin(name string) {
 	league := f.GetLeague()
+	player := league.Find(name)
 
-	for i, player := range league {
-		if player.Name == name {
-			league[i].Wins++
-		}
+	if player != nil {
+		player.Wins++
 	}
 
 	f.database.Seek(0, io.SeekStart)
